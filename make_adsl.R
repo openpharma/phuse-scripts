a5378b7c7ae96029811cb59ffd7ef3d7833850ed
reikoch <8436725+reikoch@users.noreply.github.com>
2022-06-10 18:03:43 +0200

    adsl built from dm, suppdm, ds, ex

--- make_adsl.R
+++ make_adsl.R
@@ -1,9 +1,27 @@
 # create adsl from sdtm data
 
 library(tidyr)
+library(dplyr)
+library(lubridate)
 
-xx <- dm %>% 
-  select(-c("DOMAIN", "RFXSTDTC", "RFXENDTC", "RFICDTC", "RFPENDTC", "DTHDTC", "ARMCD", "ACTARMCD", "ACTARM", "COUNTRY",  "DMDTC", "DMDY")) %>% 
-  right_join(pivot_wider(data = studydata$suppdm, id_cols = USUBJID, names_from = QNAM, values_from = QVAL, values_fill = 'N')) %>% 
-  rename(saffl=SAFETY, ittfl=ITT, efffl=EFFICACY, comp8fl=COMPLT8, comp16fl=COMPLT16, comp24fl=COMPLT24 ) %>% 
-  left_join((ds %>% filter(DSSEQ==1) %>% select(USUBJID, DSDECOD) %>% mutate(dcreascd=str_to_title(DSDECOD), .keep='unused')), by='USUBJID')
+adsl <- sdtm$dm %>%
+  mutate(TRTSDT=as_date(RFXSTDTC),
+         TRTEDT=as_date(RFXENDTC),
+         TRTDUR=1+time_length(TRTEDT-TRTSDT, unit='days'),
+         RFENDT=as_date(RFENDTC)
+         ) %>%
+  select(-c("DOMAIN", "RFXSTDTC", "RFXENDTC", "RFICDTC", "RFPENDTC", "DTHDTC", "ARMCD", "ACTARMCD", "ACTARM", "COUNTRY",  "DMDTC", "DMDY")) %>%
+  right_join(pivot_wider(data = sdtm$suppdm, id_cols = USUBJID, names_from = QNAM, values_from = QVAL, values_fill = 'N')) %>%
+  rename(SAFFL=SAFETY, ITTFL=ITT, EFFFL=EFFICACY, COMP8FL=COMPLT8, COMP16FL=COMPLT16, COMP24FL=COMPLT24 ) %>%
+  left_join((sdtm$ds %>%
+               filter(DSSEQ==1) %>%
+               select(USUBJID, DSDECOD) %>%
+               mutate(DCREASCD=str_to_title(DSDECOD)) %>%
+               rename(DCDECOD=DSDECOD)
+               ), by='USUBJID') %>%
+  left_join((sdtm$ex %>%
+               mutate(dur=1+EXENDY-EXSTDY) %>%
+               group_by(USUBJID) %>%
+               summarise(CUMDOSE=sum(EXDOSE*dur), TRTDUR=sum(dur)) %>%
+               mutate(AVGDD=CUMDOSE/TRTDUR)
+               ))
--- read_study.R
+++ read_study.R
@@ -3,5 +3,8 @@
 library(haven)
 library(stringr)
 
-studydata <- sapply(list.files(path='data/sdtm/cdiscpilot01', pattern = '.xpt$', full.names = TRUE), read_xpt, simplify = FALSE)
-names(studydata) <- str_replace(basename(names(studydata)), '.xpt$', '')
+sdtm <- sapply(list.files(path='data/sdtm/cdiscpilot01', pattern = '.xpt$', full.names = TRUE), read_xpt, simplify = FALSE)
+names(sdtm) <- str_replace(basename(names(sdtm)), '.xpt$', '')
+
+adam <- sapply(list.files(path='data/adam/cdiscpilot01', pattern = '.xpt$', full.names = TRUE), read_xpt, simplify = FALSE)
+names(adam) <- str_replace(basename(names(adam)), '.xpt$', '')
