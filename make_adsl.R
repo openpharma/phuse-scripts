c0728ebb1cf1ca333c8d207953c36f2d4aab907b
reikoch <8436725+reikoch@users.noreply.github.com>
2022-06-16 10:38:49 +0200

    fix missing trtedt

--- make_adsl.R
+++ make_adsl.R
@@ -6,12 +6,13 @@ library(lubridate)
 
 adsl <- sdtm$dm %>%
   mutate(TRTSDT=as_date(RFXSTDTC),
-         TRTEDT=as_date(RFXENDTC),
+         TRTEDT=coalesce(as_date(RFXENDTC), as_date(RFENDTC)),
          TRTDUR=1+time_length(TRTEDT-TRTSDT, unit='days'),
          RFENDT=as_date(RFENDTC)
          ) %>%
   select(-c("DOMAIN", "RFXSTDTC", "RFXENDTC", "RFICDTC", "RFPENDTC", "DTHDTC", "ARMCD", "ACTARMCD", "ACTARM", "COUNTRY",  "DMDTC", "DMDY")) %>%
-  right_join(pivot_wider(data = sdtm$suppdm, id_cols = USUBJID, names_from = QNAM, values_from = QVAL, values_fill = 'N')) %>%
+  right_join(pivot_wider(data = sdtm$suppdm, id_cols = USUBJID, names_from = QNAM, values_from = QVAL, values_fill = 'N'),
+             by='USUBJID') %>%
   rename(SAFFL=SAFETY, ITTFL=ITT, EFFFL=EFFICACY, COMP8FL=COMPLT8, COMP16FL=COMPLT16, COMP24FL=COMPLT24 ) %>%
   left_join((sdtm$ds %>%
                filter(DSSEQ==1) %>%
@@ -22,9 +23,9 @@ adsl <- sdtm$dm %>%
   left_join((sdtm$ex %>%
                mutate(dur=1+EXENDY-EXSTDY) %>%
                group_by(USUBJID) %>%
-               summarise(CUMDOSE=sum(EXDOSE*dur), TRTDUR=sum(dur)) %>%
-               mutate(AVGDD=CUMDOSE/TRTDUR)
-               )) %>%
+               summarise(CUMDOSE=coalesce(sum(EXDOSE*dur),0)) #%>% #, TRTDUR=coalesce(sum(dur), 0)) %>%
+               # mutate(AVGDD=coalesce(CUMDOSE/TRTDUR), 0)
+               ), by='USUBJID') %>%
   left_join(sdtm$vs %>%
               select(USUBJID, VISITNUM, VSTESTCD, VSSTRESN) %>%
               filter(VISITNUM %in% c(1,3) & VSTESTCD %in% c('HEIGHT', 'WEIGHT')) %>%
@@ -33,4 +34,4 @@ adsl <- sdtm$dm %>%
               fill(HEIGHT, WEIGHT) %>%
               filter(row_number()==n()) %>%
               mutate(BMIBL = WEIGHT/((HEIGHT/100) * (HEIGHT/100)), WEIGHTBL=WEIGHT, HEIGHTBL=HEIGHT) %>%
-              select(USUBJID, BMIBL, WEIGHTBL, HEIGHTBL))
+              select(USUBJID, BMIBL, WEIGHTBL, HEIGHTBL), by='USUBJID')
